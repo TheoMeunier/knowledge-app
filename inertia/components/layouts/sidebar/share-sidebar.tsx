220f0acef674bc3a493d27567e890c67b571ce8a
tmeunier <theo.meunier41@gmail.com>
2026-03-03 09:17:55 +0100

    fix: share sidebar

--- inertia/components/layouts/sidebar/share-sidebar.tsx
+++ inertia/components/layouts/sidebar/share-sidebar.tsx
@@ -1,4 +1,13 @@
-import { Sidebar, SidebarHeader, SidebarRail } from '@/components/ui/sidebar'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarRail,
+} from '@/components/ui/sidebar'
 import { LibraryBig } from 'lucide-react'
 import { usePage } from '@inertiajs/react'
 import { TreeShare } from '@/pages/shares/list_collapsible'
@@ -20,14 +29,25 @@ export default function ShareSidebar() {
   const { props } = usePage<SharePageProps>()
 
   return (
-    <Sidebar {...props}>
+    <Sidebar>
       <SidebarHeader className="flex flex-row items-center justify-center py-5 gap-3">
         <div className="bg-primary text-primary-foreground flex size-7 items-center justify-center rounded-md">
           <LibraryBig className="size-6" />
         </div>
         <h1 className="text-2xl font-semibold">Knowledge</h1>
       </SidebarHeader>
-      <TreeShare item={props.tree_share} share={props.tree_share.share!!} />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="flex justify-between">Tree</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {props.tree_share.folders.map((folder) => (
+                <TreeShare item={folder} key={folder.id} share={props.tree_share.share} />
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
       <SidebarRail />
     </Sidebar>
   )
