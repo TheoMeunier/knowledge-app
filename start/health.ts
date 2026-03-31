a7878ec97b6e12513de3ff9d8286d6521e8d9ee2
tmeunier <theo.meunier41@gmail.com>
2026-03-31 15:16:18 +0200

    feat(health): add health check endpoints and configuration

---
+++ start/health.ts
@@ -0,0 +1,9 @@
+import { DiskSpaceCheck, HealthChecks, MemoryHeapCheck } from '@adonisjs/core/health'
+import db from '@adonisjs/lucid/services/db'
+import { DbCheck } from '@adonisjs/lucid/database'
+
+export const healthChecks = new HealthChecks().register([
+  new DiskSpaceCheck(),
+  new MemoryHeapCheck(),
+  new DbCheck(db.connection()),
+])
